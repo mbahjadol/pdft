bdc96bec0ff21d65bb68ef1664bf09c23774930c
mbahjadol <mbah.jadol@gmail.com>
2025-05-21 09:07:21 +0700

    remove unnecessary fmt.Sprint

--- content_line_style.go
+++ content_line_style.go
@@ -15,11 +15,11 @@ func (c *ContentLineStyle) toSteram() (*bytes.Buffer, error) {
 	var buff bytes.Buffer
 	switch c.lineType {
 	case "dashed":
-		buff.WriteString(fmt.Sprint("[5] 2 d\n"))
+		buff.WriteString("[5] 2 d\n")
 	case "dotted":
-		buff.WriteString(fmt.Sprint("[2 3] 11 d\n"))
+		buff.WriteString("[2 3] 11 d\n")
 	default:
-		buff.WriteString(fmt.Sprint("[] 0 d\n"))
+		buff.WriteString("[] 0 d\n")
 	}
 	buff.WriteString(fmt.Sprintf("%.2f w\n", c.width))
 	return &buff, nil
